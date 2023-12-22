01c1fbf61bac07b5ca9b16b6fa4cf5bde8de9ada
Tombstone1922 <114335234+Tombstone1922@users.noreply.github.com>
2023-12-22 06:17:34 +0400

    Update Items.js

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
+// Items.js
+import React from 'react';
 import Item from './Item';
+import { useFeatureFlag } from '../FeatureFlagContext';
 
-class Items extends Component {
-  render() {
-    if (!this.props.items || this.props.items.length === 0) {
-      return (
-        <div className="empty">
-          <h2>Товаров нет</h2>
-        </div>
-      );
-    }
+const Items = (props) => {
+  const { showItems } = useFeatureFlag();
 
+  if (!showItems) {
+    return null;
+  }
+
+  if (!props.items || props.items.length === 0) {
     return (
-      <main>
-        {this.props.items.map(el => (
-          <Item key={el.id} item={el} onAdd={this.props.onAdd} onShowItem={this.props.onShowItem} />
-        ))}
-      </main>
+      <div className="empty">
+        <h2>Товаров нет</h2>
+      </div>
     );
   }
-}
+
+  return (
+    <main>
+      {props.items.map((el) => (
+        <Item key={el.id} item={el} onAdd={props.onAdd} onShowItem={props.onShowItem} />
+      ))}
+    </main>
+  );
+};
 
 export default Items;
